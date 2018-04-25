0142ba5c588d9fd2319ea60c55396045783b4c47
Nicole Hall <nhall@pairin.com>
2018-04-25 15:23:02 -0600

    adds array of items and CartItems component loops through them and displays them. Formatting is off however

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import CartItem from './CartItem'
 
-const CartItems = () => {
+const CartItems = ({cartItems}) => {
+  let individualItems = cartItems.map(item => <CartItem key={ item.id } item={item} />)
   return (
     <div className="container">
       <h1>Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        { individualItems }
+      </div>
     </div>
   )
 }
