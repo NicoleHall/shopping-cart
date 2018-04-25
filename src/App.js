c1398f8adfd9a234563dcb3afa93a0285c7af6c4
Nicole Hall <nhall@pairin.com>
2018-04-25 10:29:58 -0600

    commits Lesson 3 of React Components course. Includes Header component, Footer component and CartItems component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CartItems from './components/CartItems';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <CartItems/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
